30fd26ba9f46e836fd51ccb62623c864bdbbcf6e
Zosia <sophia.dybek@gmail.com>
2020-08-20 22:16:39 +0200

    drobne poprawki losowania i css

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const fotkiKaczek = ["img/kaczka1.png", "img/kaczka2.jpg", "img/kaczka3.jpg", "i
 
 const losowanieKaczki = () => {
     kaczkaContainter.innerHTML = "<img src='" + fotkiKaczek[Math.floor(Math.random() * fotkiKaczek.length)] + "'></img>";
-    console.log(fotkiKaczek[Math.floor(Math.random() * fotkiKaczek.length)]);
     console.log(kaczkaContainter.innerHTML);
 
 }
