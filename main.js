423c3a4d97bbb3b13b63cabf990b80913a1852f7
Janek Janiak <janek.janiak@gmail.com>
2020-08-21 10:33:54 +0200

    Zmiana w funkcji zmianaKaczki

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const losuj = document.querySelector('button');
-const kaczkaContainter = document.querySelector('#glownaKaczka')
+let kaczkaContainter = document.querySelector('#glownaKaczka')
 
 const fotkiKaczek = ["img/kaczka1.png", "img/kaczka2.jpg", "img/kaczka3.jpg", "img/kaczka4.jpg", "img/kaczka5.jpg", "img/kaczka6.jpg"]
 
@@ -8,7 +8,7 @@ const zmianaKaczki=()=>{
         let kaczkaIndex = [Math.floor(Math.random() * fotkiKaczek.length)];
         console.log(kaczkaIndex);
         console.log(fotkiKaczek[kaczkaIndex]);
-        return fotkiKaczek[kaczkaIndex];
+        kaczkaContainter.innerHTML = "<img src='" + fotkiKaczek[kaczkaIndex] + "'></img>"
 }
 
 losuj.addEventListener('click', zmianaKaczki)
