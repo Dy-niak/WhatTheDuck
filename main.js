7551c0bbd256a60ef9653b5d736ae5de33b9afe5
Janek Janiak <janek.janiak@gmail.com>
2020-08-21 17:41:47 +0200

    Whole new main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const losuj = document.querySelector('button');
-let kaczkaContainter = document.querySelector('#glownaKaczka')
+const kaczkaContainter = document.querySelector('#glownaKaczka')
+const bazaKaczek = ["img/kaczka1.png", "img/kaczka2.jpg", "img/kaczka3.jpg", "img/kaczka4.jpg", "img/kaczka5.jpg", "img/kaczka6.jpg"];
+let fotkiKaczek = bazaKaczek.concat();
+let kaczkaCounter=0;
 
-const fotkiKaczek = ["img/kaczka1.png", "img/kaczka2.jpg", "img/kaczka3.jpg", "img/kaczka4.jpg", "img/kaczka5.jpg", "img/kaczka6.jpg"]
-
-
-const zmianaKaczki=()=>{
+const aktywnaKaczka=()=>{
         let kaczkaIndex = [Math.floor(Math.random() * fotkiKaczek.length)];
-        console.log(kaczkaIndex);
-        console.log(fotkiKaczek[kaczkaIndex]);
-        kaczkaContainter.innerHTML = "<img src='" + fotkiKaczek[kaczkaIndex] + "'></img>"
+        kaczkaContainter.innerHTML = "<img src='" + fotkiKaczek[kaczkaIndex] + "'></img>";
+        fotkiKaczek.splice(kaczkaIndex,1);
+        kaczkaCounter++;
+}
+
+let zmianaKaczki=()=>{
+        if(kaczkaCounter==bazaKaczek.length){
+                fotkiKaczek=fotkiKaczek.concat(bazaKaczek);
+                kaczkaCounter=0;
+                aktywnaKaczka();
+        }else{
+                aktywnaKaczka();
+        }
 }
 
 losuj.addEventListener('click', zmianaKaczki)
