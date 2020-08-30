6941583ad79a25f30b15d4bf5300d4b70653a073
Zosia <sophia.dybek@gmail.com>
2020-08-30 20:06:10 +0200

    dodanie If statement, dzięki któremu wyświetla się console log, kiedy skończy się odliczanie

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const draw = document.querySelector('button');
-const duckContainter = document.querySelector('#glownaKaczka')
+const duckContainter = document.querySelector('.glownaKaczka')
 const duckHome = ["img/kaczka1.png", "img/kaczka2.jpg", "img/kaczka3.jpg", "img/kaczka4.jpg", "img/kaczka5.jpg", "img/kaczka6.jpg"];
 let duckPond = duckHome.concat();
 let duckCounter=0;
+
+
+//Losowanie kaczki
 
 const activeDuck=()=>{
         let duckIndex = [Math.floor(Math.random() * duckPond.length)];
@@ -26,31 +29,25 @@ draw.addEventListener('click', newDuck)
 
 //Odliczanie do nowej kaczki
 
-        // The data/time we want to countdown to
-        var tomorrow = new Date(2030, 08, 23, 15, 26, 10);
+const tomorrow = new Date(2030, 08, 30, 20, 03, 20);
 
-        // Run myfunc every second
-        var myfunc = setInterval(function() {
+const odliczanie = () => {
+        const now = new Date().getTime();
+        const timeleft = tomorrow - now;
+        const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-        var now = new Date().getTime();;
-        var timeleft = tomorrow - now;
-        
-        // Calculating the days, hours, minutes and seconds left
-        var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-        
-        // Result is output to the specific element
         document.getElementById("hours").innerHTML = hours + "h " 
         document.getElementById("mins").innerHTML = minutes + "m " 
-        document.getElementById("secs").innerHTML = seconds + "s " 
-        
-        // Display the message when countdown is over
-        if (timeleft < 0) {
-        clearInterval(myfunc);
-        document.getElementById("hours").innerHTML = "" 
-        document.getElementById("mins").innerHTML = ""
-        document.getElementById("secs").innerHTML = ""
-        document.getElementById("end").innerHTML = "TIME UP!!";
+        document.getElementById("secs").innerHTML = seconds + "s "
+
+        if ((hours === 0) && (minutes === 0 &&seconds === 0)){
+                console.log("PODMIANA KACZKI DNIA");
         }
-        }, 1000);
+}       
+
+setInterval(odliczanie, 1000);
+
+
+
